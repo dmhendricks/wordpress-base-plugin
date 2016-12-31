f5fa1a962cf56a1c62921542025844de6d243128
Daniel Hendricks <mail@danhendricks.com>
2016-12-31 16:44:56 -0600

    Code cleanup

--- wordpress-base-plugin.php
+++ wordpress-base-plugin.php
@@ -28,8 +28,6 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-define("_PLUGIN_NAMESPACE", 'Nimbium\MyPlugin');
-
 require( __DIR__ . '/vendor/autoload.php' );
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
