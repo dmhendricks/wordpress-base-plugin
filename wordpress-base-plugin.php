ca62290dbea999004234bc212f56fd97af133b28
Daniel Hendricks <mail@danhendricks.com>
2017-01-10 16:02:11 -0600

    [Minor] Added ABSPATH exit

--- wordpress-base-plugin.php
+++ wordpress-base-plugin.php
@@ -27,6 +27,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+if(!defined('ABSPATH')) exit;
 
 require( __DIR__ . '/vendor/autoload.php' );
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
