c579a5070ca4443c34f8c65f710323335d63ead4
Daniel Hendricks <mail@danhendricks.com>
2017-01-17 09:37:00 -0600

    Added example composer repository for loading a library from a ZIP file URL

--- src/EnqueueScripts.php
+++ src/EnqueueScripts.php
@@ -11,14 +11,26 @@ class EnqueueScripts extends Plugin {
         });
     }
 
-    // Enqueue admin scripts
+    /**
+      * Enqueue scripts used on frontend of site
+      */
     private static function enqueue_frontend_scripts() {
 
-      wp_enqueue_style( 'custom-style', plugins_url('/assets/css/site.css', dirname(__FILE__)), array('parent-style'), self::get_script_modified_version('../assets/css/site.css') );
+      // Example enqueuing remote scripts (http://select2.github.io/)
+      wp_enqueue_style( 'select2', '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css', false, '4.0.3' );
+      wp_enqueue_script( 'select2', '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js', array('jquery'), self::get_script_modified_version('../assets/css/site.css', '1.8.3') );
 
+      // Example enqueuing a script added from ZIP file via composer (http://underscorejs.org/)
+      wp_enqueue_script( 'underscore', plugins_url('/vendor/jashkenas/underscore/underscore-min.js', dirname(__FILE__)), false, '1.8.3' );
+
+      // Enqueuing custom CSS for child theme (Twentysixteen was used for testing)
+      wp_enqueue_style( 'child-style', plugins_url('/assets/css/site.css', dirname(__FILE__)), array('parent-style', 'select2'), self::get_script_modified_version('../assets/css/site.css') );
+      wp_enqueue_script( 'select2', '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js', array('jquery'), self::get_script_modified_version('../assets/css/site.css', '1.8.3') );
     }
 
-    // Enqueue admin scripts
+    /**
+      * Enqueue scripts used in WP admin interface
+      */
     private static function enqueue_admin_scripts() {
 
         // Only load script(s) on edit pages
@@ -35,9 +47,31 @@ class EnqueueScripts extends Plugin {
 
     }
 
-    private static function get_script_modified_version($script) {
-      if(!defined('WP_ENV')) return parent::get_option('data')['Version'];
-      return WP_ENV == 'production' ? parent::get_option('data')['Version'] : date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . $script ));
+    /**
+      * Returns script ?ver= version based on environment (WP_ENV)
+      *
+      * If WP_ENV == 'production', returns @param string $script_version (if specified),
+      * else, returns $plugin_version, else if WP_ENV is equal to anything else, returns
+      * string representing file last modified change (to prevent caching during development).
+      *
+      * @param string $script The filesystem path (relative to the script location of calling
+      *    script) to return the version for.
+      * @param string $script_version (optional) The version that will be returned if
+      *    WP_ENV == 'production'. If not specified, plugin version will be used.
+      *
+      * @return string
+      */
+    private static function get_script_modified_version($script, $script_version = null) {
+      $plugin_version = parent::get_option('data')['Version'];
+      if(!defined('WP_ENV')) return $plugin_version;
+
+      try {
+        $script_version = WP_ENV == 'production' ? ($script_version ? $script_version : $plugin_version) : date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . $script ));
+      } catch (Exception $e) {
+        $script_version = $plugin_version;
+      }
+
+      return $script_version;
     }
 
 }
