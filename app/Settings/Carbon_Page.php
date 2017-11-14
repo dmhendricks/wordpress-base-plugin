9a7bf2ab20b9665dc4124f931a0164d703252699
Daniel Hendricks <mail@danhendricks.com>
2017-11-14 14:13:17 -0600

    Minor cleanup of CF settings page

--- app/Settings/Carbon_Page.php
+++ app/Settings/Carbon_Page.php
@@ -21,7 +21,7 @@ class Carbon_Page extends Plugin {
     $this->settings_containers = array();
 
     // Flush the cache when settings are saved
-    add_action('carbon_fields_theme_options_container_saved', array( $this, 'options_saved_hook' ) );
+    add_action( 'carbon_fields_theme_options_container_saved', array( $this, 'options_saved_hook' ) );
 
     // Create tabbed plugin options page (Settings > Plugin Name)
     $this->create_tabbed_options_page();
@@ -42,61 +42,62 @@ class Carbon_Page extends Plugin {
 
     // Carbon Fields Docs: https://carbonfields.net/docs/containers-theme-options/
     $container = Container::make( 'theme_options', self::$config->get( 'plugin/meta/Name' ) )
-      ->set_page_parent('options-general.php')
-      ->add_tab( __('General', self::$textdomain), array(
-        Field::make('checkbox', $this->prefix('uninstall_remove_settings'), __('Delete Plugin Settings On Uninstall', self::$textdomain) ),
-        Field::make('checkbox', $this->prefix('admin_bar_add_clear_cache'), __('Add "Clear Cache" Link to Admin Bar Dropdown Menu', self::$textdomain) )->set_default_value(true),
-        Field::make('checkbox', $this->prefix('remove_header_emojicons'), __('Remove Emoji Code From Page Headers', self::$textdomain) )
-          ->help_text( __('Checking this box will remove the default Emoji code from page headers.', self::$textdomain) ),
-        Field::make( 'set', $this->prefix('enqueue_font_awesome'), __('Load Font Awesome from CDN', self::$textdomain) )
-          ->help_text( __('Load <a href="http://fontawesome.io/" target="_blank">Font Awesome</a> from <a href="https://www.bootstrapcdn.com/fontawesome/" target="_blank">CDN</a>.', self::$textdomain) )
-          ->add_options(array(
-            'frontend' => __('Frontend', self::$textdomain),
-            'backend' => __('Backend', self::$textdomain)
-          ))
-          ->set_default_value('backend'),
-        Field::make( 'separator', $this->prefix('general_separator_examples'), __('Example Fields', self::$textdomain) )
-          ->help_text( __('These fields are just provided as examples and are not used by any logic in the plugin.', self::$textdomain) ),
-        Field::make('text', $this->prefix('blog_title'), __('Blog Title', self::$textdomain)),
-        Field::make('text', $this->prefix('email'), __('Your E-mail Address', self::$textdomain) )
-          ->set_attribute('type', 'email')
-          ->help_text( __('This input field is an HTML5 <tt>email</tt> type.', self::$textdomain) ),
-        Field::make('text', $this->prefix('web_site_url'), __('Web Site Address', self::$textdomain) )
-          ->set_attribute('type', 'url')->set_attribute( 'placeholder', site_url() )
-          ->help_text( __('This input field is an HTML5 <tt>url</tt> type.', self::$textdomain) ),
-        Field::make('text', $this->prefix('phone'), __('Phone Number', self::$textdomain) )
-          ->set_attribute('type', 'tel'),
-        Field::make('date_time', $this->prefix('date_time'), __('Date & Time', self::$textdomain) ),
-        Field::make('radio', $this->prefix('radio_buttons'), __('Menu Position', self::$textdomain))
-          ->add_options(array(
-            'none' => __('Disabled', self::$textdomain),
-            'top' => __('Top (Default)', self::$textdomain),
-            'left' => __('Left', self::$textdomain)
-          ))
-          ->set_default_value('top'),
-        Field::make('complex', $this->prefix('slides'), self::$config->get( 'plugin/meta/Name' ) . ' ' . __('Slides', self::$textdomain))
-          ->set_datastore( new Serialized_Theme_Options_Datastore() )
-          ->add_fields(array(
-            Field::make('text', 'title'),
-            Field::make('image', 'photo'),
+      ->set_page_parent( 'options-general.php' )
+      ->add_tab( __( 'General', self::$textdomain ), array(
+        Field::make( 'checkbox', $this->prefix( 'uninstall_remove_settings' ), __( 'Delete Plugin Settings On Uninstall', self::$textdomain ) ),
+        Field::make( 'checkbox', $this->prefix( 'admin_bar_add_clear_cache' ), __( 'Add "Clear Cache" Link to Admin Bar Dropdown Menu', self::$textdomain ) )->set_default_value( true ),
+        Field::make( 'checkbox', $this->prefix( 'remove_header_emojicons' ), __( 'Remove Emoji Code From Page Headers', self::$textdomain ) )
+          ->help_text( __( 'Checking this box will remove the default Emoji code from page headers.', self::$textdomain  ) ),
+        Field::make( 'set', $this->prefix( 'enqueue_font_awesome' ), __( 'Load Font Awesome from CDN', self::$textdomain ) )
+          ->help_text( __( 'Load <a href="http://fontawesome.io/" target="_blank">Font Awesome</a> from <a href="https://www.bootstrapcdn.com/fontawesome/" target="_blank">CDN</a>.', self::$textdomain ) )
+          ->set_default_value( 'backend' )
+          ->add_options( array(
+            'frontend' => __( 'Frontend', self::$textdomain ),
+            'backend' => __( 'Backend', self::$textdomain )
           )
         ),
-        Field::make('select', $this->prefix('select_dropdown'), __('Favorite Continent', self::$textdomain) )
+        Field::make( 'separator', $this->prefix( 'general_separator_examples' ), __( 'Example Fields', self::$textdomain ) )
+          ->help_text( __( 'These fields are just provided as examples and are not used by any logic in the plugin.', self::$textdomain ) ),
+        Field::make( 'text', $this->prefix( 'blog_title' ), __( 'Blog Title', self::$textdomain ) ),
+        Field::make( 'text', $this->prefix( 'email' ), __( 'Your E-mail Address', self::$textdomain ) )
+          ->set_attribute( 'type', 'email' )
+          ->help_text( __( 'This input field is an HTML5 <tt>email</tt> type.', self::$textdomain ) ),
+        Field::make( 'text', $this->prefix( 'web_site_url' ), __( 'Web Site Address', self::$textdomain ) )
+          ->set_attribute( 'type', 'url' )->set_attribute( 'placeholder', site_url() )
+          ->help_text( __( 'This input field is an HTML5 <tt>url</tt> type.', self::$textdomain ) ),
+        Field::make( 'text', $this->prefix( 'phone' ), __( 'Phone Number', self::$textdomain ) )
+          ->set_attribute( 'type', 'tel' ),
+        Field::make( 'date_time', $this->prefix( 'date_time' ), __( 'Date & Time', self::$textdomain ) ),
+        Field::make( 'radio', $this->prefix( 'radio_buttons' ), __( 'Menu Position', self::$textdomain ) )
           ->add_options(array(
-            'aria' => __('Asia', self::$textdomain),
-            'africa' => __('Africa', self::$textdomain),
-            'europe' => __('Europe', self::$textdomain),
-            'north-america' => __('North America', self::$textdomain),
-            'south-america' => __('South America', self::$textdomain),
-            'australia' => __('Australia', self::$textdomain),
-            'antarctica' => __('Antarctica', self::$textdomain)
+            'none' => __( 'Disabled', self::$textdomain ),
+            'top' => __( 'Top (Default)', self::$textdomain ),
+            'left' => __( 'Left', self::$textdomain )
+          ))
+          ->set_default_value( 'top' ),
+        Field::make( 'complex', $this->prefix( 'slides' ), self::$config->get( 'plugin/meta/Name' ) . ' ' . __( 'Slides', self::$textdomain ) )
+          ->set_datastore( new Serialized_Theme_Options_Datastore() )
+          ->add_fields( array(
+            Field::make( 'text', 'title' ),
+            Field::make( 'image', 'photo' ),
+          )
+        ),
+        Field::make( 'select', $this->prefix( 'select_dropdown' ), __( 'Favorite Continent', self::$textdomain ) )
+          ->add_options(array(
+            'aria' => __( 'Asia', self::$textdomain ),
+            'africa' => __( 'Africa', self::$textdomain ),
+            'europe' => __( 'Europe', self::$textdomain ),
+            'north-america' => __( 'North America', self::$textdomain ),
+            'south-america' => __( 'South America', self::$textdomain ),
+            'australia' => __( 'Australia', self::$textdomain ),
+            'antarctica' => __( 'Antarctica', self::$textdomain )
           ))
         )
       )
-      ->add_tab( __('Miscellaneous', self::$textdomain), array(
-        Field::make('color', $this->prefix('font_color'), __('Foreground Color', self::$textdomain) ),
-        Field::make('image', $this->prefix('default_image'), __('Default Image', self::$textdomain) ),
-        Field::make('file', $this->prefix('file'), __('File Upload', self::$textdomain) )
+      ->add_tab( __( 'Miscellaneous', self::$textdomain ), array(
+        Field::make( 'color', $this->prefix( 'font_color' ), __( 'Foreground Color', self::$textdomain ) ),
+        Field::make( 'image', $this->prefix( 'default_image' ), __( 'Default Image', self::$textdomain ) ),
+        Field::make( 'file', $this->prefix( 'file' ), __( 'File Upload', self::$textdomain ) )
       )
     );
 
@@ -113,10 +114,10 @@ class Carbon_Page extends Plugin {
   private function create_single_options_page() {
 
     Container::make( 'theme_options', self::$config->get( 'plugin/meta/Name' ) )
-      ->set_page_parent('options-general.php')
+      ->set_page_parent( 'options-general.php' )
       ->add_fields( array(
-        Field::make( 'text', $this->prefix('your_name'), __( 'Your Name', self::$textdomain) ),
-        Field::make( 'image', $this->prefix('profile_pic'), __('Profile Pic', self::$textdomain) )
+        Field::make( 'text', $this->prefix( 'your_name' ), __( 'Your Name', self::$textdomain ) ),
+        Field::make( 'image', $this->prefix( 'profile_pic' ), __( 'Profile Pic', self::$textdomain ) )
       )
     );
 
